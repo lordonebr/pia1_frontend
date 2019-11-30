c175734106291bd8c658d97a4e4aa2164510d9ee
André Lordone <andgas@yahoo.com.br>
2019-11-30 19:45:28 -0300

    add carregamento das ultimas transações

--- js/home.js
+++ js/home.js
@@ -6,63 +6,67 @@ initHome = () => {
 // recupera do WS, o histórico das últimas transações do sistema
 loadLastHist = () => {
 
-    const dateTag = "date";
-    const nameDonorTag = "nameDonor";
-    const nameReceptorTag = "nameReceptor";
-    const valueTag = "value";
+    loadFromService('GET', '/transfers')
+        .then(histJSON => {
 
-    let histJSON = [
-        {
-            [dateTag] : "15-11-2019 20:05",
-            [nameDonorTag] : "Alice",
-            [nameReceptorTag] : "Mariana",
-            [valueTag] : 7
-        },
-        {
-            [dateTag] : "15-11-2019 18:00",
-            [nameDonorTag] : "André",
-            [nameReceptorTag] : "Mariana",
-            [valueTag] : 5
-        },
-        {
-            [dateTag] : "15-11-2019 16:43",
-            [nameDonorTag] : "André",
-            [nameReceptorTag] : "Alice",
-            [valueTag] : 10
-        },
-        {
-            [dateTag] : "15-11-2019 13:57",
-            [nameDonorTag] : "Mariana",
-            [nameReceptorTag] : "André",
-            [valueTag] : 8
-        }
-    ]
+            const dateTag = "date";
+            const nameDonorTag = "nameSender";
+            const nameReceptorTag = "nameRecipient";
+            const valueTag = "value";
 
-    let element = document.getElementById("idLastHist");
-    if(element){
+            /*histJSON = [
+                {
+                    [dateTag] : "15-11-2019 20:05",
+                    [nameDonorTag] : "Alice",
+                    [nameReceptorTag] : "Mariana",
+                    [valueTag] : 7
+                },
+                {
+                    [dateTag] : "15-11-2019 18:00",
+                    [nameDonorTag] : "André",
+                    [nameReceptorTag] : "Mariana",
+                    [valueTag] : 5
+                },
+                {
+                    [dateTag] : "15-11-2019 16:43",
+                    [nameDonorTag] : "André",
+                    [nameReceptorTag] : "Alice",
+                    [valueTag] : 10
+                },
+                {
+                    [dateTag] : "15-11-2019 13:57",
+                    [nameDonorTag] : "Mariana",
+                    [nameReceptorTag] : "André",
+                    [valueTag] : 8
+                }
+            ]*/
 
-        // limpa conteudo antigo
-        for(let i = element.rows.length - 1; i > 0; i--)
-            element.deleteRow(i);
+            let element = document.getElementById("idLastHist");
+            if(element){
 
-        // preenche com o conteudo novo vindo do WS
-        for(let idx in histJSON){
+                // limpa conteudo antigo
+                for(let i = element.rows.length - 1; i > 0; i--)
+                    element.deleteRow(i);
 
-            if(histJSON[idx].hasOwnProperty(dateTag) &&
-               histJSON[idx].hasOwnProperty(nameDonorTag) &&
-               histJSON[idx].hasOwnProperty(nameReceptorTag) &&
-               histJSON[idx].hasOwnProperty(valueTag)
-            ){
-                let NewRow = element.insertRow(-1);
-                let Newcell1 = NewRow.insertCell(0); 
-                let Newcell2 = NewRow.insertCell(1);
-                let Newcell3 = NewRow.insertCell(2); 
-                let Newcell4 = NewRow.insertCell(3); 
-                Newcell1.innerHTML = histJSON[idx][dateTag]; 
-                Newcell2.innerHTML = histJSON[idx][nameDonorTag]; 
-                Newcell3.innerHTML = histJSON[idx][nameReceptorTag]; 
-                Newcell4.innerHTML = histJSON[idx][valueTag]; 
+                // preenche com o conteudo novo vindo do WS
+                for(let idx in histJSON){
+
+                    if(histJSON[idx].hasOwnProperty(dateTag) &&
+                    histJSON[idx].hasOwnProperty(nameDonorTag) &&
+                    histJSON[idx].hasOwnProperty(nameReceptorTag) &&
+                    histJSON[idx].hasOwnProperty(valueTag)
+                    ){
+                        let NewRow = element.insertRow(-1);
+                        let Newcell1 = NewRow.insertCell(0); 
+                        let Newcell2 = NewRow.insertCell(1);
+                        let Newcell3 = NewRow.insertCell(2); 
+                        let Newcell4 = NewRow.insertCell(3); 
+                        Newcell1.innerHTML = histJSON[idx][dateTag]; 
+                        Newcell2.innerHTML = histJSON[idx][nameDonorTag]; 
+                        Newcell3.innerHTML = histJSON[idx][nameReceptorTag]; 
+                        Newcell4.innerHTML = histJSON[idx][valueTag]; 
+                    }
+                }
             }
-        }
-    }
+        });
 }
